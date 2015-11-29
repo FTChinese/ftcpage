1f5cbb079d562ac43751d1064ac776a6c0f03a9d
Oliver Zhang <oli.zhang@gmail.com>
2015-11-29 10:54:18 +0800

    update double click server list

--- app/scripts/track-third-party.js
+++ app/scripts/track-third-party.js
@@ -26,98 +26,96 @@ function checkAd(adOptions, adDomContainer) {
   // to be on the safe side
   // use setTimeout so that the third party script will be 
   // captured in the innerHTML in all browsers
-  setTimeout (function(){
-    var img;
-    var passDomCheck = false;
-    var adDomImgs = 0;
-    var adDomObjects = 0;
-    var adDomIFrames = 0;
-    var adDomVideos = 0;
-    var adName = '';
-    var fallbackImgContainer = adDomContainer.getElementsByTagName('div')[0];
-    var thirdPartyVendor = '';
-    var reachabilityStatus = ''; // reachable, unreachable, unknown
-    var cookieSeconds = 60 * 60;
-    if (adOptions.checking === true) {
-      adName = adOptions.adClient + ' ' + adOptions.adWidth + 'x' + adOptions.adHeight + ' ' + adOptions.adNote;
-      if (adOptions.checkingTime === 0) {
-        if (adDomContainer.innerHTML.indexOf('doubleclick')>0) {
-          thirdPartyVendor = 'dcR';
-        } else {
-          trackAd('Impression Track Start', adName, 'novendor');
-        }
-        if (thirdPartyVendor !== '') {
-          // Check reachability status
-          reachabilityStatus = GetCookie (thirdPartyVendor) || 'unknown';
-          adOptions.reachabilityStatus = reachabilityStatus;
-          adOptions.thirdPartyVendor = thirdPartyVendor;
-        }
-        trackAd('Impression Track Start', adName, reachabilityStatus);
-        //SetCookie(thirdPartyVendor, 'unknown', cookieSeconds, '/');
-      } else {
-        if (adOptions.reachabilityStatus !== undefined ) {
-          reachabilityStatus = adOptions.reachabilityStatus;
-        //console.log (reachabilityStatus);
-        }
-        if (adOptions.thirdPartyVendor !== undefined) {
-          thirdPartyVendor = adOptions.thirdPartyVendor;
-        }
+  var img;
+  var passDomCheck = false;
+  var adDomImgs = 0;
+  var adDomObjects = 0;
+  var adDomIFrames = 0;
+  var adDomVideos = 0;
+  var adName = '';
+  var fallbackImgContainer = adDomContainer.getElementsByTagName('div')[0];
+  var thirdPartyVendor = '';
+  var reachabilityStatus = ''; // reachable, unreachable, unknown
+  var cookieSeconds = 60 * 59;
+  if (adOptions.checking === true) {
+    adName = adOptions.adClient + ' ' + adOptions.adWidth + 'x' + adOptions.adHeight + ' ' + adOptions.adNote;
+    if (adOptions.thirdPartyVendor !== undefined) {
+      thirdPartyVendor = adOptions.thirdPartyVendor;
+    } else if (/doubleclick|adsafeprotected\.com\/rfw\/dc/i.test(adOptions.fallBackImg)) {
+      thirdPartyVendor = 'dcR';
+    }
+    if (adOptions.checkingTime === 0) {
+      if (thirdPartyVendor !== '') {
+        // Check reachability status
+        reachabilityStatus = GetCookie (thirdPartyVendor) || 'unknown';
+        adOptions.reachabilityStatus = reachabilityStatus;
+        //adOptions.thirdPartyVendor = thirdPartyVendor;
       }
-      //console.log (thirdPartyVendor);
-      //Dom Check
-      if (adDomContainer === null) {
-        passDomCheck = false;
-        //console.log ('no dom yet');
-      } else if (adOptions.checkDom === true) {
-        adDomImgs = adDomContainer.getElementsByTagName('img').length;
-        adDomObjects = adDomContainer.getElementsByTagName('object').length;
-        adDomIFrames = adDomContainer.getElementsByTagName('iframe').length;
-        adDomVideos = adDomContainer.getElementsByTagName('video').length;
-        if (adDomImgs > 0 || adDomIFrames > 0 || adDomObjects > 0 || adDomVideos > 0) {
-          passDomCheck = true;
-        } else {
-          passDomCheck = false;
-        }
-      } else {
-        passDomCheck = true;
+      trackAd('Impression Track Start', adName, reachabilityStatus);
+      //SetCookie(thirdPartyVendor, 'unknown', cookieSeconds, '/');
+    } else {
+      if (adOptions.reachabilityStatus !== undefined ) {
+        reachabilityStatus = adOptions.reachabilityStatus;
+      //console.log (reachabilityStatus);
       }
-      if (passDomCheck === true) {
-        adOptions.checking = false;
-        trackAd('Impression Track Success', adName, reachabilityStatus);
-        SetCookie(thirdPartyVendor, 'reachable', cookieSeconds, '/');
-      } else if (adOptions.checkingTime < adOptions.checkingTimeMax) {
-        //else if we still have time to check
-        //check again in 1 second
-        adOptions.checkingTime += 1;
-        setTimeout(function(){
-          checkAd(adOptions, adDomContainer);
-        }, 1000);
-      } else {
-        //else if time runs out
-        //tell Google Analytics ad has failed to load
-        //load the fallback image
-        trackAd('Impression Track Fail Main', adName, reachabilityStatus);
-        SetCookie(thirdPartyVendor, 'unreachable', cookieSeconds, '/');
-        if (adOptions.fallBackImg !== undefined && adOptions.fallBackImg !== '') {
-          img = new Image();
-          img.src = adOptions.fallBackImg; 
-          img.onload = function() {
-            //fallback image is loaded successfully
-            //tell Google Analytics that ad has loaded through the fallback
-            //adDomContainer.style.backgroundImage = 'url('+ img.src +')';
-            fallbackImgContainer.innerHTML = '<img src="' + img.src + '">';
-            trackAd('Impression Track Success', adName, reachabilityStatus);
-            trackAd('Impression Track Fallback Image', adName, reachabilityStatus);
-          };
-          img.onerror = function() {
-            //fallback image is not loaded successfully
-            //tell Google Analytics that fallback image has also failed
-            trackAd('Impression Track Fail Fallback', adName, reachabilityStatus);
-          };
-        }
+      if (adOptions.thirdPartyVendor !== undefined) {
+        thirdPartyVendor = adOptions.thirdPartyVendor;
       }
     }
-  },10);
+    //console.log (thirdPartyVendor);
+    //Dom Check
+    if (adDomContainer === null) {
+      passDomCheck = false;
+      //console.log ('no dom yet');
+    } else if (adOptions.checkDom === true) {
+      adDomImgs = adDomContainer.getElementsByTagName('img').length;
+      adDomObjects = adDomContainer.getElementsByTagName('object').length;
+      adDomIFrames = adDomContainer.getElementsByTagName('iframe').length;
+      adDomVideos = adDomContainer.getElementsByTagName('video').length;
+      if (adDomImgs > 0 || adDomIFrames > 0 || adDomObjects > 0 || adDomVideos > 0) {
+        passDomCheck = true;
+      } else {
+        passDomCheck = false;
+      }
+    } else {
+      passDomCheck = true;
+    }
+    if (passDomCheck === true) {
+      adOptions.checking = false;
+      trackAd('Impression Track Success', adName, reachabilityStatus);
+      SetCookie(thirdPartyVendor, 'reachable', cookieSeconds, '/');
+    } else if (adOptions.checkingTime < adOptions.checkingTimeMax) {
+      //else if we still have time to check
+      //check again in 1 second
+      adOptions.checkingTime += 1;
+      setTimeout(function(){
+        checkAd(adOptions, adDomContainer);
+      }, 1000);
+    } else {
+      //else if time runs out
+      //tell Google Analytics ad has failed to load
+      //load the fallback image
+      trackAd('Impression Track Fail Main', adName, reachabilityStatus);
+      SetCookie(thirdPartyVendor, 'unreachable', cookieSeconds, '/');
+      if (adOptions.fallBackImg !== undefined && adOptions.fallBackImg !== '') {
+        img = new Image();
+        img.src = adOptions.fallBackImg; 
+        img.onload = function() {
+          //fallback image is loaded successfully
+          //tell Google Analytics that ad has loaded through the fallback
+          //adDomContainer.style.backgroundImage = 'url('+ img.src +')';
+          fallbackImgContainer.innerHTML = '<img src="' + img.src + '">';
+          trackAd('Impression Track Success', adName, reachabilityStatus);
+          trackAd('Impression Track Fallback Image', adName, reachabilityStatus);
+        };
+        img.onerror = function() {
+          //fallback image is not loaded successfully
+          //tell Google Analytics that fallback image has also failed
+          trackAd('Impression Track Fail Fallback', adName, reachabilityStatus);
+        };
+      }
+    }
+  }
 }
 
 function checkAdLoad() {
